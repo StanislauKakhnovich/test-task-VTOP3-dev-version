082a68a662479b136bc6c7462798acaa63bb4204
Stanislau Kakhnovich <stas_2002@tut.by>
2022-07-28 16:44:40 +0300

    bacground-image right

--- gulpfile.js
+++ gulpfile.js
@@ -19,17 +19,6 @@ function browsersync() {
 
 function images(){
     return src('app/images/**/*')
-    .pipe(imagemin([
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.mozjpeg({quality: 75, progressive: true}),
-        imagemin.optipng({optimizationLevel: 5}),
-        imagemin.svgo({
-            plugins: [
-                {removeViewBox: true},
-                {cleanupIDs: false}
-            ]
-        })
-    ]))
     .pipe(dest('dist/images'))
 }
 
